2b2241336516b8905a2c757e09231d5bbd3d3953
DMartynas <martynas.diedonis@ymail.com>
2018-09-20 21:58:54 +0300

    added ETH and LTC

--- app/code/community/Mage/Coingate/Model/ReceiveCurrencies.php
+++ app/code/community/Mage/Coingate/Model/ReceiveCurrencies.php
@@ -8,6 +8,8 @@ class Mage_Coingate_Model_ReceiveCurrencies
             array('value' => 'btc', 'label' => 'Bitcoin (฿)'),
             array('value' => 'eur', 'label' => 'Euros (€)'),
             array('value' => 'usd', 'label' => 'US Dollars ($)'),
+            array('value' => 'eth', 'label' => 'Etherium'),
+            array('value' => 'ltc', 'label' => 'Litecoin'),
             array('value' => 'DO_NOT_CONVERT', 'label' => 'Do not convert')
         );
     }
